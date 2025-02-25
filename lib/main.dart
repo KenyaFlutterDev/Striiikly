9a716f2fe4afa4b0a883da3525766ab77837f727
bensalcie <bensalcie@gmail.com>
2025-02-25 23:25:20 +0300

    Adds Necessary Dependancies, themes, folders, .env files

--- lib/main.dart
+++ lib/main.dart
@@ -11,18 +11,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Striikly Hureeeey',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Striikly Hureeeey'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
 
   final String title;
 
